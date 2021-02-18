aac07121e5f47553f4bf96fc888ecf3b37f9fc7e
dmb225 <daomalick225@gmail.com>
2021-02-18 13:03:44 +0100

    Add a restful api to access subjects

--- educa/settings.py
+++ educa/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'students.apps.StudentsConfig',
     'embed_video',
     'memcache_status',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -137,4 +138,11 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11212',
     }
+}
+
+# rest
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
 }
