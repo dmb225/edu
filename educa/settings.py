9a5106a5ac47f03c59aadf280b7065257b87c802
dmb225 <daomalick225@gmail.com>
2021-02-20 15:15:30 +0100

    Writing a chat consumer using django channels

--- educa/settings.py
+++ educa/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'memcache_status',
     'rest_framework',
     'chat',
+    'channels',
 ]
 
 MIDDLEWARE = [
@@ -147,3 +148,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+ASGI_APPLICATION = 'educa.routing.application'
